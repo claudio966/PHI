8e91d5f277dcab3c1db4b421574e512b19934833
Bruno Martins <63007760+Euronym@users.noreply.github.com>
2022-04-12 14:15:25 -0300

    Update somador.vhd

--- roteiro_2/somador.vhd
+++ roteiro_2/somador.vhd
@@ -7,20 +7,20 @@ use IEEE.std_logic_unsigned.all;
 entity somador is
 	
 	generic(
-		nbits: integer := 4
+		nbits: integer := 4 -- valor default
 	);
 	
 	port(
-		a : in std_logic_vector (nbits - 1 downto 0);
-		b : in std_logic_vector (nbits - 1 downto 0);
-		resultado_somador: out std_logic_vector (nbits - 1 downto 0)
+		a : in std_logic_vector (nbits - 1 downto 0); -- operando de entrada
+		b : in std_logic_vector (nbits - 1 downto 0); -- operando de entrada
+		resultado_somador: out std_logic_vector (nbits - 1 downto 0) -- valor a ser exibido na saida do somador
 	);
 end somador;
 
 architecture comportamento of somador is
 begin
-	process(a, b)
+	process(a, b) -- inicializa o processo para a operação de soma, o gatilho é a inserção dos valores 
 	begin
-		resultado_somador <= a + b;
+		resultado_somador <= a + b; -- atribui a soma A + B a saida do somador
 	end process;
 end comportamento;
