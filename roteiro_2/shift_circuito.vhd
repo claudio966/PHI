dae3c148a20e9188b26efa3d8147c70b6c0e270d
Bruno Martins <63007760+Euronym@users.noreply.github.com>
2022-04-12 14:16:55 -0300

    Update shift_circuito.vhd

--- roteiro_2/shift_circuito.vhd
+++ roteiro_2/shift_circuito.vhd
@@ -9,19 +9,19 @@ use IEEE.std_logic_unsigned.all;
 
 entity shift is 
 	generic(
-		nbits: integer := 4
+		nbits: integer := 4 -- valor default
 	);
 	port(
-		a : in std_logic_vector (nbits - 1 downto 0);
-		b : in std_logic_vector (nbits - 1 downto 0);
-		resultado_shift: out std_logic_vector (nbits - 1 downto 0)
+		a : in std_logic_vector (nbits - 1 downto 0); -- entrada do circuito
+		b : in std_logic_vector (nbits - 1 downto 0); -- entrada do circuito
+		resultado_shift: out std_logic_vector (nbits - 1 downto 0) -- saida do circuito
 	);
 end shift;
 
 architecture comportamento of shift is
 	begin
-	process(a,b) 
+	process(a,b) -- declaracao do processo. O gatilho é a inserção dos operandos
 	begin
-		resultado_shift <= std_logic_vector(ROTATE_LEFT(unsigned(a), unsigned(b) ));	
+		resultado_shift <= std_logic_vector(ROTATE_LEFT(unsigned(a), unsigned(b) )); -- realiza a operacao de rotacao
 	end process;
 end comportamento;
