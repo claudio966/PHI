c0ca9687d83ac35d1cd0df1a75ae11b50d809542
claudio966 <claudiomatheus966@gmail.com>
2022-05-07 10:52:39 -0300

    multiplexador, subtrator e somador adicionados

--- atividade_2/bo.vhd
+++ atividade_2/bo.vhd
@@ -1,20 +1,48 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity registrador is 
 	generic (
 		nbits : integer := 8;
 	);
 	port (
+		clk : in std_logic;
 		reg_load : in std_logic;
 		reg_clear : in std_logic;
 		output : out std_logic_vector(nbits - 1 downto 0);
+		input : in std_logic_vector(nbits - 1 downto 0);
+		output : in std_logic_vector(nbits - 1 downto 0);
+		alu : in std_logic_vector(1 downto 0);
+		ie : in st_logic;
 	);
 end registrador;
 
 architeture comportamento of registrador is
+	signal item_preco : std_logic_vector(nbits - 1 downto 0) := "00000000";
 begin
+	--multiplexador
+	mux_output <= input when ie = '1' else 1;
+
+	--ALU
+	process(mux_output, reg_output, alu)
+	begin
+		case alu is
+			when "00" =>
+				alu_output <= mux_output;
+			when "01" =>
+				alu_output <= std_logic_vector(signed(mux_output) + signed(reg_output));
+			when "10"
+				alu_output <= std_logic_vector(signed(mux_output) - signed(reg_output));
+			-- TODO implementar o comparador
+			-- when "11"
+			-- alu_output <= resultado_comparador	
+		end case;
+	end process;	
+
+	--registrador
 	process(clk)
+	begin
 		if rising_edge(clk) then
 			if reg_clear = '1' then
 				reg_output <= (others => '0');
@@ -22,4 +50,5 @@ begin
 				reg_output <= alu_output;
 			endif;
 		endif;
+	end process;
 end comportamento;
