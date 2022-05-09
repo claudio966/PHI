d633c27f048d37868ad7aea8019cf33a6f021144
claudio966 <claudiomatheus966@gmail.com>
2022-05-09 10:58:57 -0300

    Trocado o nome do sinal clock por clk

--- atividade_2/bc_testbench.vhd
+++ atividade_2/bc_testbench.vhd
@@ -9,14 +9,14 @@ architecture comportamento of bc_testbench is
 	
 	signal moeda: std_logic_vector(7 downto 0) := "00000100";
 	signal reset: std_logic := '0';
-	signal clock: std_logic := '0';
+	signal clk: std_logic := '0';
 	signal a: std_logic_vector(7 downto 0);
 	signal c: std_logic := '0';
 	
 	component bc
 		port(
 			reset: in std_logic; -- entrada de reset
-			clock: in std_logic; -- entrada de clock
+			clk: in std_logic; -- entrada de clk
 			moeda: in std_logic_vector(7 downto 0); -- entrada correspondente ao valor monetário
 			c: out std_logic; -- saída do detector de moedas
 			a: out std_logic_vector(7 downto 0) -- saida do registrador
@@ -25,13 +25,13 @@ architecture comportamento of bc_testbench is
 	
 	begin
 	
-		clock <= '1' after 0.5 ns when clock = '0' else
-			'0' after 0.5 ns when clock ='1';
+		clk <= '1' after 0.5 ns when clk = '0' else
+			'0' after 0.5 ns when clk ='1';
 		
 		bc_test: bc
 		port map(
 			reset => reset,
-			clock => clock,
+			clk => clk,
 			moeda => moeda,
 			a => a,
 			c => c
