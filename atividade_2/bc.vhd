8f4c478bbe102fcf911cc4085200273e9ee2d99d
claudio966 <claudiomatheus966@gmail.com>
2022-05-08 20:05:32 -0400

    Trocado o nome da porta clock para clk

--- atividade_2/bc.vhd
+++ atividade_2/bc.vhd
@@ -6,7 +6,7 @@ entity bc is
 
 	port(
 		reset: in std_logic := '0'; -- entrada de reset
-		clock: in std_logic; -- entrada de clock
+		clk: in std_logic; -- entrada de clk
 		moeda: in std_logic_vector(7 downto 0); -- entrada correspondente ao valor monetário
 		c: out std_logic; -- saída do detector de moedas
 		a: out std_logic_vector(7 downto 0) -- saida do registrador
@@ -42,30 +42,30 @@ architecture comportamento of bc is
 		end case;
 	end process;
 	-- processo responsável por modificar o valor do sinal 
-	detector_moeda: process(clock, moeda)
+	detector_moeda: process(clk, moeda)
 	begin
-		if rising_edge(clock) then
+		if rising_edge(clk) then
 			com_moeda <= '1';	
 		end if;
 	end process;
 	-- processo responsavel pela mudança de estado
-	registrador_estado: process(clock, reset)
+	registrador_estado: process(clk, reset)
 	begin 
 		if reset = '1' then
 			estado_atual <= S0;
-		elsif rising_edge(clock) then
+		elsif rising_edge(clk) then
 			estado_atual <= proximo_estado;
 		end if;
 	end process;
 	-- passa o valor numerico para a saida 
-	registrador_saida: process(clock, estado_atual)
+	registrador_saida: process(clk, estado_atual)
 	begin
 		case estado_atual is
 			when S0 =>
 				a <= "00000000";
 				c <= '0';
 			when S1 => 
-				if rising_edge(clock) then
+				if rising_edge(clk) then
 					a <= moeda;
 					c <= com_moeda;
 				end if;
