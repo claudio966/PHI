447c7274c425fecf4a7ddb600dc5e158d0acffcc
claudio966 <claudiomatheus966@gmail.com>
2022-05-09 21:23:16 -0400

    adicionada a detecção de múltiplas moedas

--- atividade_2/bo.vhd
+++ atividade_2/bo.vhd
@@ -12,15 +12,15 @@ entity bo is
 		c : in std_logic; -- Entrada da moeda detectada
 		reg_clear : in std_logic;
 		a : in std_logic_vector(nbits - 1 downto 0); -- Saida do registrado de BC
-		alu : in std_logic_vector(1 downto 0);
-		liberado : out std_logic; -- o valor de d
+		--alu : in std_logic_vector(1 downto 0);
+		liberado : out std_logic := '0'; -- o valor de d
 		ie : in std_logic
 	);
 end bo;
 
 architecture comportamento of bo is
 	signal reg_output : std_logic_vector(nbits - 1 downto 0) := "00000000";
-	signal alu_output : std_logic_vector(nbits - 1 downto 0);
+	signal alu_output : std_logic_vector(nbits - 1 downto 0) := "11111111";
 	signal item_preco : std_logic_vector(nbits - 1 downto 0) := "01100100"; -- 100 centavos
 	signal mux_output : std_logic_vector(nbits - 1 downto 0);
 begin
@@ -28,31 +28,22 @@ begin
 	mux_output <= a when ie = '1' else "00000000";
 
 	--ALU
-	process(mux_output, reg_output, alu)
+	process(mux_output, reg_output)
 	begin
-		case alu is
-			when "00" =>
-				alu_output <= mux_output;
-			when "01" =>
-				alu_output <= std_logic_vector(signed(mux_output) + signed(reg_output));
-			when "10" =>
-				alu_output <= std_logic_vector(signed(mux_output) - signed(reg_output));
-			when "11" =>
-				if signed(a) >= signed(item_preco) then
-					alu_output <= "00000001";
-				else
-					alu_output <= "00000000";
-				end if;
-			when others =>
-				alu_output <= (others => '0');
-		end case;
+		if c = '1' then
+			alu_output <= std_logic_vector(signed(mux_output) + signed(reg_output));
+		end if;
+
+		if reg_output >= item_preco then
+			alu_output <= "00000000";
+		end if;
 	end process;	
 	
 	--Logica para liberar o item
 	liberar_item: process(clk)
 	begin
-		if rising_edge(clk) then
-			if reg_output = "00000001" then
+		if falling_edge(clk) then
+			if alu_output = "00000000" then
 				liberado <= '1';
 			else 
 				liberado <= '0';
