5dffe8d296f95cd51c50927e2d444bc8275ad135
Bruno <brunos-martins@hotmail.com>
2022-05-11 21:59:20 -0300

    adicao do vetor de moedas para teste

--- atividade_2/sd_testbench.vhd
+++ atividade_2/sd_testbench.vhd
@@ -15,14 +15,20 @@ architecture testbench of sd_testbench is
 			liberado : out std_logic
 		);
 	end component;
-
+	
+	type array_declaration is array (0 to 3) of std_logic_vector(7 downto 0);
+	
+	signal moeda_array: array_declaration := ("00001010", "00010100", "00011110", "00101000");
+	
+	signal i: integer := 0;
 	signal clk : std_logic := '1';
 	signal reg_clear : std_logic := '0';
-	signal moeda : std_logic_vector(7 downto 0) := "00011001";
+	signal moeda : std_logic_vector(7 downto 0) := "00000000";
 	signal ie: std_logic := '1';
 	signal liberado: std_logic := '0';
 	
 begin
+
 	clk <= '1' after 0.5 ns when clk = '0' else
 		'0' after 0.5 ns when clk ='1';
 	
@@ -34,4 +40,15 @@ begin
 		ie => ie,
 		liberado => liberado
 	);
+	inserir_moeda: process(clk)
+	begin
+		if falling_edge(clk) then
+			if i > 3 then
+				i <= 0;
+			else
+				moeda <= moeda_array(i);
+				i <= i + 1;
+			end if;
+		end if;
+	end process;	
 end architecture;
